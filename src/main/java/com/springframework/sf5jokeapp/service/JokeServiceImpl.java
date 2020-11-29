880c1a9b4b3bd202ad9cd58a3951b9f3b7a32a71
amin rezazade <amin_r359@yahoo.com>
2020-11-29 20:12:53 +0330

    making better joke service with insctructor tip

--- src/main/java/com/springframework/sf5jokeapp/service/JokeServiceImpl.java
+++ src/main/java/com/springframework/sf5jokeapp/service/JokeServiceImpl.java
@@ -6,8 +6,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JokeServiceImpl implements JokeService {
+    private final  ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
     @Override
     public String getRandJoke() {
-        return new ChuckNorrisQuotes().getRandomQuote();
+        return chuckNorrisQuotes.getRandomQuote();
     }
 }
