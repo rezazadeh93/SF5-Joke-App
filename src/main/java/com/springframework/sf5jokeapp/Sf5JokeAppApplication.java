65dac2537732e29c2fd39ff56339cadab01a326e
amin rezazade <amin_r359@yahoo.com>
2020-11-29 14:48:03 +0330

    initiate project repository

---
+++ src/main/java/com/springframework/sf5jokeapp/Sf5JokeAppApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.sf5jokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sf5JokeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sf5JokeAppApplication.class, args);
+    }
+
+}
