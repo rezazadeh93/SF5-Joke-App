6201086b32380a2f0a31a3d68558adb651a851a5
Amin <amin.rezazadeh93@gmail.com>
2020-12-04 00:01:48 +0330

    adding configuration to chuck norris external jar

--- src/main/java/com/springframework/sf5jokeapp/service/JokeServiceImpl.java
+++ src/main/java/com/springframework/sf5jokeapp/service/JokeServiceImpl.java
@@ -1,15 +1,14 @@
 package com.springframework.sf5jokeapp.service;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JokeServiceImpl implements JokeService {
     private final  ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
